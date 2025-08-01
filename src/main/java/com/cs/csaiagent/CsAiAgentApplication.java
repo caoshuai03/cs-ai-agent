cb67848b554debd7bc8a2b3259761cbab9ad887d
caoshuai.cs <shuaicao01@163.com>
2025-08-01 14:44:11 +0800

    fix:优化

--- src/main/java/com/cs/csaiagent/CsAiAgentApplication.java
+++ src/main/java/com/cs/csaiagent/CsAiAgentApplication.java
@@ -8,6 +8,13 @@ public class CsAiAgentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CsAiAgentApplication.class, args);
+        System.out.println("" +
+                "   ____ ____    _    ___    _                    _   \n" +
+                "  / ___/ ___|  / \\  |_ _|  / \\   __ _  ___ _ __ | |_ \n" +
+                " | |   \\___ \\ / _ \\  | |  / _ \\ / _` |/ _ \\ '_ \\| __|\n" +
+                " | |___ ___) / ___ \\ | | / ___ \\ (_| |  __/ | | | |_ \n" +
+                "  \\____|____/_/   \\_\\___/_/   \\_\\__, |\\___|_| |_|\\__|\n" +
+                "                                |___/                ");
     }
 
 }
